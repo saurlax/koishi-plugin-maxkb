dd93bf13948eeb26bf3da0eaefc39fa33ba102af
Saurlax <saurlax@qq.com>
2024-10-02 15:32:01 +0800

    feat: integrate maxkb

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,78 @@
-import { Context, Schema } from 'koishi'
+import { posix } from "path";
+import { Context, Schema } from "koishi";
 
-export const name = 'maxkb'
+export const name = "maxkb";
 
-export interface Config {}
+export const inject = ["database", "http"];
 
-export const Config: Schema<Config> = Schema.object({})
+export interface Config {
+  baseUrl: string;
+  apikey: string;
+  atTrigger: boolean;
+  keywordTrigger: boolean;
+  keyword: string;
+}
+
+export const Config: Schema<Config> = Schema.object({
+  baseUrl: Schema.string().description("MaxKB 应用的 Base URL"),
+  apikey: Schema.string().description("MaxKB 应用的 API Key").role("secret"),
+  atTrigger: Schema.boolean().default(true).description("@机器人触发对话"),
+  keywordTrigger: Schema.boolean()
+    .default(false)
+    .description("检测关键词触发对话"),
+  keyword: Schema.string().description("检测关键词的正则表达式"),
+});
+
+export interface MaxKBChat {
+  id: string;
+  chatId: string;
+}
+
+declare module "koishi" {
+  interface Tables {
+    maxkb: MaxKBChat;
+  }
+}
+
+async function openChat(ctx: Context) {
+  const url = posix.join(ctx.config.baseUrl, "chat/open");
+  const res = await ctx.http.get(url, {
+    headers: { Authorization: ctx.config.apikey },
+  });
+  return res.data;
+}
+
+async function chat(ctx: Context, message: string, chatId: string) {
+  const url = posix.join(ctx.config.baseUrl, "../chat_message", chatId);
+  const res = await ctx.http.post(
+    url,
+    { message, stream: false },
+    { headers: { Authorization: ctx.config.apikey } }
+  );
+  return res.data.content;
+}
 
 export function apply(ctx: Context) {
-  // write your plugin here
+  const keyword = new RegExp(ctx.config.keyword);
+  ctx.model.extend("maxkb", {
+    id: "string",
+    chatId: "string",
+  });
+
+  ctx.on("message", async (session) => {
+    if (
+      (session.stripped.atSelf && ctx.config.atTrigger) ||
+      (ctx.config.keywordTrigger && session.content.match(keyword))
+    ) {
+      const chats = await ctx.database.get("maxkb", session.channelId);
+      let chatId: string;
+      if (chats.length > 0) {
+        chatId = chats[0].chatId;
+      } else {
+        chatId = await openChat(ctx);
+        await ctx.database.create("maxkb", { id: session.channelId, chatId });
+      }
+      session.send(await chat(ctx, session.content, chatId));
+    }
+  });
 }
